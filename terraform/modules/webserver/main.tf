5c800892442a92c26f4ba8c09b5154eee8955bc0
yash10 <yshevkar10@gmail.com>
2024-03-01 17:26:03 +0530

    updated

--- terraform/modules/webserver/main.tf
+++ terraform/modules/webserver/main.tf
@@ -58,7 +58,7 @@ resource "aws_instance" "web" {
    associate_public_ip_address=true
    subnet_id=var.subnet_id
   vpc_security_group_ids = [aws_security_group.mywebsecurity.id]
-   key_name="ansible-key"
+   key_name="phpdb_key"
   tags = {
     Name = "${var.env}-instance"
   }
