4b494b072c06b0356c0b4058c6a5ba18a7ed649d
yash10 <yshevkar10@gmail.com>
2024-03-03 17:47:54 +0530

    updated

--- ansible/prepare-ACM.sh
+++ ansible/prepare-ACM.sh
@@ -3,8 +3,9 @@
 pip3 install boto3
 aws configure set aws_access_key_id $1
 aws configure set aws_secret_access_key $2
-/home/ec2-user/.local/bin/ansible-inventory -i /home/ec2-user/inventory_aws_ec2.yml --graph
 chmod 400 /home/ec2-user/.ssh/id_rsa
+/home/ec2-user/.local/bin/ansible-inventory -i /home/ec2-user/inventory_aws_ec2.yml --graph
+
 #cat docker-compose.yml
 #cat compose-playbook.yml
 /home/ec2-user/.local/bin/ansible-playbook -i /home/ec2-user/inventory_aws_ec2.yml  /home/ec2-user/compose-playbook.yml -e "DOCKER_IMAGE=$4 DOCKER_REG_PASSWORD=$3"
